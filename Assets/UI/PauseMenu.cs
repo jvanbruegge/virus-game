93ff9735ccfa886b1672dd2265ced694798663e0
Jan van Brügge <supermanitu@gmail.com>
2020-07-11 15:21:28 +0200

    Add basic music

---
+++ Assets/UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+    [SerializeField]
+    private GameObject pauseMenu;
+    [SerializeField]
+    private Slider volumeSlider;
+    [SerializeField]
+    private AudioSource music;
+
+    private bool isActive = false;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            this.ToggleMenu();
+        }
+    }
+
+    public void ToggleMenu() {
+        this.isActive = !isActive;
+
+        if (isActive) {
+            Time.timeScale = 0;
+        } else {
+            Time.timeScale = 1;
+        }
+        pauseMenu.SetActive(isActive);
+
+    }
+
+    public void UpdateVolume() {
+        music.volume = volumeSlider.value;
+    }
+}
