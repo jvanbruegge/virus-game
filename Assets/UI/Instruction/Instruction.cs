262fb4bbd90b87ac8745162073262e1aef4f56cf
Jan van Brügge <supermanitu@gmail.com>
2020-07-11 11:27:09 +0200

    Add basic UI layout

---
+++ Assets/UI/Instruction/Instruction.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * The value of these have to match the file name
+ */
+public enum EInstruction {
+    FWD,
+    BWD,
+    Left,
+    Right
+}
+
+public class Instruction : MonoBehaviour {
+    public EInstruction instruction;
+    private static GameObject prefab = null;
+
+    private void Start() {
+        this.GetComponent<Image>().sprite = Resources.Load<Sprite>("Instruction/" + instruction);
+    }
+
+    public static GameObject Create(EInstruction instruction, Transform parent) {
+        if(prefab == null) {
+            prefab = Resources.Load("Instruction/Instruction") as GameObject;
+        }
+        GameObject obj = Instantiate(prefab, parent);
+        obj.GetComponent<Instruction>().instruction = instruction;
+        return obj;
+    }
+}
---
+++ Assets/UI/InstructionList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class InstructionList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instruction.Create(EInstruction.FWD, transform);
+        Instruction.Create(EInstruction.BWD, transform);
+        Instruction.Create(EInstruction.Left, transform);
+        Instruction.Create(EInstruction.Right, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
